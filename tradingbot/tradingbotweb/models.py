407e6ffd818e07177fccb0b704b585f88caa3199
amina129 <monceramina5@gmail.com>
2025-06-24 18:26:02 +0100

    created currency and transaction models , linked the app to the project ,created objects from the admin UI

--- tradingbot/tradingbotweb/models.py
+++ tradingbot/tradingbotweb/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Currency (models.Model):
+    symbol     = models.CharField(max_length=10,primary_key=True)
+    usd_value=models.DecimalField(max_digits=10, decimal_places=2, default=1.0 )
+    
+class Transaction(models.Model):
+    origin_currency = models.ForeignKey('Currency',on_delete=models.CASCADE, related_name='origin_currency')
+    destination_curency = models.ForeignKey('Currency', on_delete=models.CASCADE, related_name='destination_currency')
+    original_currency_value= models.DecimalField(max_digits=10, decimal_places=2)
+    destination_currency_value = models.DecimalField(max_digits=10, decimal_places=2)
+    comission = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
+    exchange_date = models.DateTimeField(auto_now_add=True)
+    
