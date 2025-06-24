5d279f8ee825a125b8cf9829878b6fe94e5d2db7
amina129 <monceramina5@gmail.com>
2025-06-24 20:10:41 +0100

    added API and currency history model

--- tradingbot/tradingbotweb/admin.py
+++ tradingbot/tradingbotweb/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
-from .models import Currency, Transaction
+from .models import Currency, Transaction, CurrencyHistory
+
+class CurrencyHistoryAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_at',)
+
+class TransactionAdmin(admin.ModelAdmin):
+    readonly_fields = ('exchange_date',)
 
 admin.site.register(Currency)
-admin.site.register(Transaction)
+admin.site.register(Transaction, TransactionAdmin)
+admin.site.register(CurrencyHistory, CurrencyHistoryAdmin)
--- tradingbot/tradingbotweb/models.py
+++ tradingbot/tradingbotweb/models.py
@@ -1,14 +1,34 @@
 from django.db import models
+import requests
+import json
+from decimal import Decimal
 
-class Currency (models.Model):
-    symbol     = models.CharField(max_length=10,primary_key=True)
-    usd_value=models.DecimalField(max_digits=10, decimal_places=2, default=1.0 )
-    
+class Currency(models.Model):
+    symbol = models.CharField(max_length=10, primary_key=True)
+    usd_value = models.DecimalField(max_digits=10, decimal_places=2, default=1.0)
+
 class Transaction(models.Model):
-    origin_currency = models.ForeignKey('Currency',on_delete=models.CASCADE, related_name='origin_currency')
-    destination_curency = models.ForeignKey('Currency', on_delete=models.CASCADE, related_name='destination_currency')
-    original_currency_value= models.DecimalField(max_digits=10, decimal_places=2)
+    origin_currency = models.ForeignKey('Currency', on_delete=models.CASCADE, related_name='origin_currency')
+    destination_currency = models.ForeignKey('Currency', on_delete=models.CASCADE, related_name='destination_currency')
+    original_currency_value = models.DecimalField(max_digits=10, decimal_places=2)
     destination_currency_value = models.DecimalField(max_digits=10, decimal_places=2)
-    comission = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
+    commission = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
     exchange_date = models.DateTimeField(auto_now_add=True)
-    
+
+class CurrencyHistory(models.Model):
+    symbol = models.ForeignKey('Currency', on_delete=models.CASCADE)
+    usd_value = models.DecimalField(max_digits=10, decimal_places=6, default=1.0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        self.usd_value = self.get_value()
+        super().save(*args, **kwargs)
+
+    def get_value(self):
+        api_url = f'https://economia.awesomeapi.com.br/last/{self.symbol.symbol}-USD'
+        response = requests.get(api_url).content
+        json_data = json.loads(response)
+        usd_ask_value = json_data[f'{self.symbol.symbol}USD']['ask']
+        decimal_value = Decimal(usd_ask_value)  # Use Decimal imported from decimal
+        return decimal_value
+
